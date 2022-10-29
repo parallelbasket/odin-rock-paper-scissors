a79f9a070df6273bdc70fa9b8e441c710c7ce899
parallelbasket <a.milevskiy111@gmail.comgit config --global user.email a.milevskiy111@gmail.com>
2022-10-30 09:54:50 +1000

    Add play round function. takes the player selection and the computer selection as parameters. Compares the paramenters to find if the game is a win, lose or a draw. Returns reult as a string.

--- javascript.js
+++ javascript.js
@@ -25,3 +25,28 @@ function makePlayerSelection() {
     choice = choice.toLowerCase();
     return choice;
 }
+
+// Play Round
+// Takes computer selection and player selection as parameters
+// Compares selections to determine winner
+// 
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        return "Draw";
+    } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
+        return "You win!";
+    } else if (playerSelection == 'rock' && computerSelection == 'paper') {
+        return "You lose!";
+    } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+        return "You win!";
+    } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
+        return "You lose!";
+    } else if (playerSelection == 'paper' && computerSelection == 'rock') {
+        return "You win!";
+    } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
+        return "You lose!";
+    }
+}
+
+console.log(playRound(getComputerChoice(), makePlayerSelection()));
