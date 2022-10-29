b0c8711dc77487c28e1590d1caed4a36c8a36182
parallelbasket <a.milevskiy111@gmail.comgit config --global user.email a.milevskiy111@gmail.com>
2022-10-30 09:29:29 +1000

    Create get computer choice function. Function makes the computer select 'rock', 'paper' or 'scissors'.

---
+++ javascript.js
@@ -0,0 +1,16 @@
+// Get Computer Choice
+// Create variable 'choice'
+// Create a random number between 0 & 2
+// Use that number to select 'rock' 'paper' or 'scissors'
+// return 'choice'
+
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 3);
+    switch (choice) {
+        case 0: choice = 'rock'; break;
+        case 1: choice = 'paper'; break;
+        case 2: choice = 'scissors'; break;
+    }
+    return choice;
+}
+console.log(getComputerChoice());
